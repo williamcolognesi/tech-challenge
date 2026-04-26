3de1ca8db146374725733a8ca21dc45ffa1b7bed
Queity <queity.ss@gmail.com>
2026-04-26 20:57:23 -0300

    feat: adiciona variável de ambiente USE_MOCK para alternar entre mock e banco de dados
    - adiciona USE_MOCK no transaction.factory.ts
    - atualiza .env.example com a nova variável

--- lib/factories/transaction.factory.ts
+++ lib/factories/transaction.factory.ts
@@ -1,8 +1,11 @@
 import { PrismaTransactionRepository } from '@/features/transactions/repositories/transaction.repository.prisma';
 import { TransactionService } from '@/features/transactions/services/transaction.service';
-//import { MockTransactionRepository } from '@/mocks/transaction/transaction.repository.mock';
+import { MockTransactionRepository } from '@/mocks/transaction/transaction.repository.mock';
 
-//const repository = new MockTransactionRepository();
-const repository = new PrismaTransactionRepository();
+const useMock = process.env.USE_MOCK === 'true';
+
+const repository = useMock
+    ? new MockTransactionRepository()
+    : new PrismaTransactionRepository();
 
 export const transactionService = new TransactionService(repository);
